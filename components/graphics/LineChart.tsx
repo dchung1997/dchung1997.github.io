fe37d6dc4b69808b0b462136ec531f1e4b308440
David <davidmchung123@gmail.com>
2024-10-17 06:05:10 -0500

    Update LineChart.tsx

--- components/graphics/LineChart.tsx
+++ components/graphics/LineChart.tsx
@@ -32,15 +32,21 @@ const LineChart = ({data, extent, width, height, title, subtitle, caption, x, y,
                 stroke: "url(#gradient-" + id + ")",
                 strokeWidth: 2.5,
               }),    
-              (_index, { y }) => htl.svg`<defs>
-              <linearGradient id="gradient-${id}" gradientUnits="userSpaceOnUse"
-                x1=0 x2=0 y1=${y(extent[0])} y2=${y(extent[1])}>${d3.ticks(0, 1, 10).map(
-                (t) => {
-                  return htl.svg`<stop
-                            offset=${t * 100}%
-                            stop-color=${color(extent[0] * (1 - t) + extent[1] * t)} />`                    
+              (_index, { y }) => {
+                if (!_index || !y) {
+                  return null; // Return null if _index or y is undefined
                 }
-              )}`              
+                
+                return htl.svg`<defs>
+                <linearGradient id="gradient-${id}" gradientUnits="userSpaceOnUse"
+                  x1=0 x2=0 y1=${y(extent[0])} y2=${y(extent[1])}>${d3.ticks(0, 1, 10).map(
+                  (t) => {
+                    return htl.svg`<stop
+                              offset=${t * 100}%
+                              stop-color=${color(extent[0] * (1 - t) + extent[1] * t)} />`                    
+                  }
+                )}`
+              }              
             ]
         });
         plotRef.current.appendChild(plot);
