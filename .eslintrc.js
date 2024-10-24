e55a37a044607e13115ac0fff0cdbaa4fe2ee5b1
David <davidmchung123@gmail.com>
2024-10-23 19:08:08 -0500

    Update .eslintrc.js
    removed eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,14 +22,6 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': [
-      'error',
-      {
-        components: ['Link'],
-        specialLink: ['hrefLeft', 'hrefRight'],
-        aspects: ['invalidHref', 'preferButton'],
-      },
-    ],
     'react/prop-types': 0,
     '@typescript-eslint/no-unused-vars': 0,
     '@typescript-eslint/no-explicit-any': 0,
