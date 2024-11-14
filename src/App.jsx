697d5739f50a6c5b2e62a1501bdeebbe81540271
David <davidmchung123@gmail.com>
2024-11-14 16:57:03 -0600

    Update App.jsx
    Changed back to browser router for netlify and seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import FoodAffordability from "./pages/Food-Affordability/FoodAffordability";
 import ChildMortality from "./pages/Food-Affordability/ChildMortality";
@@ -12,7 +12,7 @@ import './App.css';
 function App() {
 
   return (
-    <HashRouter basename="/">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/food-affordability">
@@ -23,7 +23,7 @@ function App() {
           <Route path="manufacturing" element={<Manufacturing/>} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
