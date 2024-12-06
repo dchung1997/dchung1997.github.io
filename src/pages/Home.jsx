1f60456de5b5e7afe08322c38cfd596f8f7d62cf
David <davidmchung123@gmail.com>
2024-12-06 16:47:23 -0600

    Update Home.jsx
    Changed Order and Added Line Break

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,59 +58,12 @@ function Home() {
         <div className="divide-y divide-gray-200">
           <div className="space-y-2 pb-8 pt-6 md:space-y-5">
             {/* Latest Series Here / Banner / Should Only Link to Series*/}
-            <div className="gap-4 grid sm:grid-cols-1 md:grid-cols-2">
-              {/* Image left then details right. */}
-              <div>
-                <Link to="/food-affordability">
-                  <img
-                    src={`${imagePath}/series/food-affordabilty-diet.png`}
-                    className="object-cover md:w-auto"
-                  ></img>
-                </Link>
-              </div>
-              <div>
-                <article>
-                  <p className="text-1xl pb-1 text-left font-extrabold text-gray-600">
-                    Latest Series
-                  </p>
-                  <Link to="/food-affordability" className="hover:underline">
-                    <p className="text-2xl font-extrabold">
-                      Food Affordability & Undernutrition
-                    </p>
-                    <p className="pt-2 text-sm">
-                      In 2023 around 2.33 billion people around the world faced
-                      moderate to severe food insecurity. While most countries
-                      are still struggling to deal with food security in some
-                      form, in developing regions its impacts are proving to be
-                      much worse. In this series, we'll be looking at these
-                      issues and the problems many governments today are facing.
-                      Along with what they are doing and could do given
-                      additional funding.
-                    </p>
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="pt-3 text-right text-sm leading-6 text-gray-500">
-                        2024-10-31
-                      </dd>
-                    </dl>
-                  </Link>
-                </article>
-                <Articles articles={articles} />
-              </div>
-            </div>
-
-            {/* 
-              Series One Main Image then Articles 
-              Similar to before except with a list of articles on topics and read times.
-          */}
-
-            {/* Latest Series Here / Banner / Should Only Link to Series*/}
             <div className="py-24 gap-4 grid">
               {/* Image left then details right. */}
               <div>
                 <article>
                   <p className="text-1xl pb-1 text-left font-extrabold text-gray-600">
-                    Interactive Series
+                    Latest Series
                   </p>
                   <Link to="/meat">
                     <p className="text-2xl font-extrabold hover:underline" >
@@ -153,6 +106,56 @@ function Home() {
                 </article>
               </div>
             </div>
+
+            <hr/>
+
+            <div className="py-24 gap-4 grid sm:grid-cols-1 md:grid-cols-2">
+              {/* Image left then details right. */}
+              <div>
+                <Link to="/food-affordability">
+                  <img
+                    src={`${imagePath}/series/food-affordabilty-diet.png`}
+                    className="object-cover md:w-auto"
+                  ></img>
+                </Link>
+              </div>
+              <div>
+                <article>
+                  <p className="text-1xl pb-1 text-left font-extrabold text-gray-600">
+                    Interactive Series
+                  </p>
+                  <Link to="/food-affordability" className="hover:underline">
+                    <p className="text-2xl font-extrabold">
+                      Food Affordability & Undernutrition
+                    </p>
+                    <p className="pt-2 text-sm">
+                      In 2023 around 2.33 billion people around the world faced
+                      moderate to severe food insecurity. While most countries
+                      are still struggling to deal with food security in some
+                      form, in developing regions its impacts are proving to be
+                      much worse. In this series, we'll be looking at these
+                      issues and the problems many governments today are facing.
+                      Along with what they are doing and could do given
+                      additional funding.
+                    </p>
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="pt-3 text-right text-sm leading-6 text-gray-500">
+                        2024-10-31
+                      </dd>
+                    </dl>
+                  </Link>
+                </article>
+                <Articles articles={articles} />
+              </div>
+            </div>
+
+            {/* 
+              Series One Main Image then Articles 
+              Similar to before except with a list of articles on topics and read times.
+          */}
+
+
 
             {/* Something on the side of other shorts? */}
           </div>
