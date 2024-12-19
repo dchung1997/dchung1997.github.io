2312cf7114890244a3c133a4ed53669b15a73c54
David <davidmchung123@gmail.com>
2024-12-19 09:34:12 -0600

    Update MeatConsumption.jsx

--- src/pages/Meat/MeatConsumption.jsx
+++ src/pages/Meat/MeatConsumption.jsx
@@ -41,7 +41,7 @@ function MeatConsumption() {
               currently consume more than 80% of all meat produced globally.
             </p>
             <ChartWrapper>
-              <h2>Total Annual Meat Consumption</h2>
+              <h2>Total Annual Meat Consumption by Kilogram</h2>
               <h3>Income Group, World, 2023</h3>
               <TreeMap data={meat_consumption} group={"type"} />
               <span className="source">
@@ -54,13 +54,71 @@ function MeatConsumption() {
               consuming around 102kg more annually than those of low income
               ones. That's more than 220 pounds! While the jump from
               upper-middle to lower-middle income countries was around 63kg or
-              138lbs.
+              138lbs. An interesting thing to note is that as countries grew
+              wealthier the amount of Poultry, Beef, and Seafood grew
+              exponentially.
             </p>
             <ChartWrapper>
               <img
                 src={`${imagePath}/series/meat-consumption/per-capita-consumption-meat-type.png`}
               ></img>
             </ChartWrapper>
+
+            <p>
+              Previously we talked about how emissions for lower income
+              countries of animals had significantly higher amounts of CO2 per
+              Kg in comparison to more developed countries. Such as for beef,
+              but is it necessarily an issue? Not necessarily. While there are
+              outliers such as India which humanely treat cows maybe a bit too
+              much. For many countries as they develop the emissions of animals
+              is generally expected to decrease. Though there are caveats to
+              this and the transition towards this also can have many
+              implications of their own.
+            </p>
+
+            <ChartWrapper>
+              <h2>Greenhouse Gas Emissions Beef Production </h2>
+              <img
+                src={`${imagePath}/series/animal-emissions/beef-emissions.png`}
+              ></img>
+              <span className="source">
+                Source: Herrero et al., 2013 PNAS 110: 20888-20893
+              </span>
+            </ChartWrapper>
+
+            <p>
+              Let's take for example the beef production in China which
+              increased from 6.13 million metric tons in 2013 to 7.53 in 2023.
+              The average yield of meat per cattle has remained relatively the
+              same going from 146.5kg to 148.4kg of meat per cattle in the same
+              period. Over a decade the amount of greenhouse emissions from
+              these cattle has likely decreased somewhat but its hard to say if
+              it is in a meaningful way. While emissions for China are
+              relatively small in comparison to other regions its still a
+              concern due to the amount of emissions still being two to three
+              times higher than in other areas.
+            </p>
+
+            <p>
+              Another example would be in Brazil, now one of the largest
+              exporters of beef globally. Brazil has seen a considerable
+              increase in beef yields over the past decade going from 232.6kg to
+              245.0kg on average. While the country faces a lot of other issues
+              in regards to climate there is at least some form of progress
+              being made. Brazil went from 10 million metric tons in 2013 to
+              10.5 million metric tons in 2023. Similar to China it isn't
+              necessarily reducing emissions by much. In many ways it seems at
+              least that emissions of some animals can get stuck.
+            </p>
+
+            <p>
+              So what's the big deal? For many countries it's believed that
+              naturally overt time as food systems develop emissions of meat
+              products is expected to decrease. But looking at the data it shows
+              that in many ways they can get stuck. This can be problematic in a
+              lot of different ways especially for countries that struggle with
+              food insecurity which is a considerable number of them.
+            </p>
           </div>
         </div>
       </main>
